a7c874805b9d97885a772060800e1e5bd033aaf0
Eugene Dyachenko <dv.eugene@gmail.com>
2016-11-28 23:48:57 +0200

    First working version

---
+++ EDDitor/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EDDitor
+{
+    public partial class MainWindow : Window
+    {
+        List<OpenedFile> openedFiles = new List<OpenedFile>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void clickBtnCreate(object sender, RoutedEventArgs e)
+        {
+            OpenedFile newFile = new OpenedFile(closeFile);
+            spMain.Children.Add(newFile.spTextSpace);
+            openedFiles.Add(newFile);
+        }
+
+        private void clickBtnMerge(object sender, RoutedEventArgs e)
+        {
+            StringBuilder content = new StringBuilder();
+
+            foreach (var file in openedFiles)
+            {
+                if (file.ReadyToMerge)
+                {
+                    content.Append(file.Content + "\n");
+                }
+            }
+
+            if (content.ToString() != "")
+            {
+                OpenedFile newFile = new OpenedFile(closeFile, content.ToString());
+                spMain.Children.Add(newFile.spTextSpace);
+                openedFiles.Add(newFile);
+            }
+        }
+
+        //events textBlock
+        private void closeFile(object sender, RoutedEventArgs e)
+        {
+            StackPanel spClosingFile = (((sender as Button).Parent as StackPanel).Parent as StackPanel); //get needed parent element
+            
+            for (int i = 0; i < openedFiles.Count; i++)
+            {
+                if(openedFiles[i].spTextSpace == spClosingFile)
+                {
+                    spMain.Children.Remove(openedFiles[i].spTextSpace);
+                    openedFiles.Remove(openedFiles[i]);
+                }
+            }
+        }
+    }
+}
---
+++ EDDitor/OpenedFile.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace EDDitor
+{
+    class OpenedFile
+    {
+        public StackPanel spTextSpace { get; set; }
+        public bool ReadyToMerge
+        {
+            get
+            {
+                StackPanel spControl = spTextSpace.Children[0] as StackPanel;
+                CheckBox cbReadyToMerge = spControl.Children[3] as CheckBox;
+                return cbReadyToMerge.IsChecked == true;
+            }
+        }
+        public string Content
+        {
+            get
+            {
+                TextBox txtContent = spTextSpace.Children[1] as TextBox;
+                return txtContent.Text;
+            }
+            set
+            {
+                TextBox txtContent = spTextSpace.Children[1] as TextBox;
+                txtContent.Text = value;
+            }
+        }
+
+        public OpenedFile(RoutedEventHandler removeEvent, string content = "")
+        {
+            spTextSpace = new StackPanel();
+            spTextSpace.Margin = new Thickness(15);
+
+            TextBox txtFileContent = new TextBox();
+            txtFileContent.MinHeight = 50;
+            txtFileContent.AcceptsReturn = true;
+            txtFileContent.Text = content;
+
+            StackPanel spControl = new StackPanel();
+            spControl.Orientation = Orientation.Horizontal;
+            spControl.HorizontalAlignment = HorizontalAlignment.Right;
+
+            //create buttons
+            Button btnRemove = CreateButton("x");
+            btnRemove.Click += removeEvent;
+            spControl.Children.Add(btnRemove);
+
+            Button btnSave = CreateButton("s");
+            spControl.Children.Add(btnSave);
+
+            Button btnSaveAs = CreateButton("ax");
+            spControl.Children.Add(btnSaveAs);
+
+            CheckBox readyToMerger = new CheckBox();
+            readyToMerger.Margin = new Thickness(10, 0, 0, 0);
+            spControl.Children.Add(readyToMerger);
+
+            //add text and buttons to block. And add him to the main panel
+            spTextSpace.Children.Add(spControl);
+            spTextSpace.Children.Add(txtFileContent);
+        }    
+
+        private Button CreateButton(string name)
+        {
+            Button btn = new Button();
+            btn.Content = name;
+            btn.Margin = new Thickness(10, 0, 0, 0);
+            return btn;
+        }
+    }
+}
